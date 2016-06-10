61c400b6ebc2c4952f6829207991858d904d4b53
KGMakarov <nfsu_n2o@mail.ru>
2016-06-11 00:07:49 +0600

    Оформил отдельным модулем. Перенес создание объектов для тестов в сам класс.  Исправил тестирование функции setElement. Добавил тестирование неизменяемости переданных аргументов функции.

--- VectorTest/src/academit/kgm/vectortest/VectorTest.java
+++ VectorTest/src/academit/kgm/vectortest/VectorTest.java
@@ -6,48 +6,48 @@ import academit.kgm.vector.Vector;
 
 public class VectorTest {
 
-    @org.junit.Before
-    public void setUp() throws Exception {
-        double temp[] = {5, 5, 5, 5, 5};
-        Vector testVector = new Vector(temp);
-        testVector.setElement(2, 8);
-        double testElement = testVector.getElement(2);
+    private final double[] temp1 = {5, 5, 5, 5, 5};
+    private final double[] temp2 = {1, 6, 3};
+    Vector testVector1 = new Vector(temp1);
+    Vector testVector2 = new Vector(temp2);
+
+    @org.junit.Test
+    public void setElement() throws Exception {
+        testVector1.setElement(2, 8);
+        double testElement = testVector1.getElement(2);
         Assert.assertEquals(8, testElement, 0.0001);
     }
 
     @org.junit.Test
     public void testGetSize() throws Exception {
-        double temp[] = {5, 5, 5, 5, 5};
-        Vector testVector = new Vector(temp);
-        Assert.assertEquals(5, testVector.getSize());
+        Assert.assertEquals(3, testVector2.getSize());
     }
 
     @org.junit.Test
     public void testSum() throws Exception {
-        double temp1[] = {1, 6, 3};
-        double temp2[] = {1, 2};
-        Vector testVector1 = new Vector(temp1);
-        Vector testVector2 = new Vector(temp2);
-        double testRes[] = {2, 8, 3};
+        Vector sourceVector2 = new Vector(testVector2);
+        Vector currentVector2 = new Vector(testVector2);
+        double testRes[] = {6, 11, 8, 5, 5};
         Vector testVectorResult = new Vector(testRes);
-        Assert.assertEquals(testVectorResult, testVector1.sum(testVector2));
+        Assert.assertEquals(testVectorResult, testVector1.sum(currentVector2));
+        Assert.assertEquals(sourceVector2,currentVector2);
     }
 
     @org.junit.Test
     public void testGetElement() throws Exception {
-        double temp1[] = {1, 6, 3};
-        Vector testVector1 = new Vector(temp1);
-        Assert.assertEquals(6, testVector1.getElement(1), 0.0001);
+        Assert.assertEquals(5, testVector1.getElement(2), 0.0001);
     }
 
     @org.junit.Test
     public void testGetSub() throws Exception {
-        double temp1[] = {1, 6, 3};
-        double temp2[] = {1, 2};
-        Vector testVector1 = new Vector(temp1);
-        Vector testVector2 = new Vector(temp2);
-        double testRes[] = {0, 4, 3};
+        Vector sourceVector1 = new Vector(testVector1);
+        Vector currentVector1 = new Vector(testVector1);
+        Vector sourceVector2 = new Vector(testVector2);
+        Vector currentVector2 = new Vector(testVector2);
+        double testRes[] = {4, -1, 2, 5, 5};
         Vector testVectorResult = new Vector(testRes);
-        Assert.assertEquals(testVectorResult, Vector.getSub(testVector1, testVector2));
+        Assert.assertEquals(testVectorResult, Vector.getSub(currentVector1, currentVector2));
+        Assert.assertEquals(sourceVector1,currentVector1);
+        Assert.assertEquals(sourceVector2,currentVector2);
     }
 }
