1dcc6a6c05cd76723192eb4cce1465e20b7e733c
KGMakarov <nfsu_n2o@mail.ru>
2016-07-01 23:56:44 +0600

    Сделал статик методы, учел замечания, добавил комментарии к коду, навел порядок в Main.

--- Matrix/src/academit/kgm/matrix/Main.java
+++ Matrix/src/academit/kgm/matrix/Main.java
@@ -6,68 +6,145 @@ import academit.kgm.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
+        // Проверка работы конструктора Matrix(n, m)
+        System.out.println("Работа конструктора Matrix(n, m):");
         Matrix x1 = new Matrix(3, 2);
         System.out.println(x1.toString());
 
+        // Проверка работы конструктора Matrix(Matrix)
+        System.out.println("Проверка работы конструктора Matrix(Matrix):");
         Matrix x2 = new Matrix(x1);
         System.out.println(x2.toString());
 
+        // Проверка работы конструктора Matrix(double[][])
+        System.out.println("Проверка работы конструктора Matrix(double[][]):");
         double[][] temp1 = {{5, 8}, {88, 5, 47}, {1, 1, 1, 1}};
         Matrix x3 = new Matrix(temp1);
         System.out.println(x3.toString());
 
+        // Проверка работы конструктора Matrix(Vector[])
+        System.out.println("Проверка работы конструктора Matrix(Vector[]):");
         double[] temp2 = {5, 8, 81};
         double[] temp3 = {5, 81, 5, 99, 99};
         double[] temp4 = {5};
         Vector[] vectorArray = {new Vector(temp2), new Vector(temp3), new Vector(temp4)};
         Matrix x4 = new Matrix(vectorArray);
-        Vector v1 = new Vector(temp4);
         System.out.println(x4.toString());
+
+        //Проверка работы метода получения размеров матрицы
+        System.out.println("Получение размеров матрицы:");
         System.out.println(x4.getNumberOfRows());
         System.out.println(x4.getNumberOfColumns());
         System.out.println(x4.getSize().toString());
+
+        //Проверка работы метода получения и задания вектора-строки по индексу
+        System.out.println("Получение и задание вектора-строки по индексу:");
+        Vector v1 = new Vector(temp4);
         System.out.println(x4.getRow(0).toString());
         x4.setRow(0, v1);
         System.out.println(x4.toString());
 
-        System.out.println();
-
+        //Проверка работы метода получения вектора-столбца по индексу
+        System.out.println("Получение вектора-столбца по индексу:");
         System.out.println(x4.getColumn(2).toString());
 
+        //Проверка работы метода транспонирования матрицы
+        System.out.println("Транспонирование матрицы:");
+
+        //Создание массивов double'ов для массива Vector'ов
         double[] temp5 = {5, 8, 81};
         double[] temp6 = {5, 81, 5, 99, 99};
         double[] temp7 = {5};
 
+        //Создание массива Vector'ов и матрицы на его основе
         Vector[] vectorArray1 = {new Vector(temp5), new Vector(temp6), new Vector(temp7)};
         Matrix x5 = new Matrix(vectorArray1);
         System.out.println(x5.toString());
         System.out.println(x5.transMatrix().toString());
 
+        //Проверка работы метода умножения на скаляр
+        System.out.println("Умножение на скаляр:");
+        System.out.println(x5.toString());
         System.out.println(x5.multiply(2).toString());
 
+        //Проверка работы метода вычисления определителя матрицы
+        System.out.println("Вычисление определителя:");
+
+        ////Создание массивов double'ов для массива Vector'ов
         double[] det0 = {2, -3, 4, 1};
         double[] det1 = {4, 0, 3, 2};
         double[] det2 = {-4, 1, 2, -2};
         double[] det3 = {3, -1, 4, 3};
+
+        //Создание массива Vector'ов и матрицы на его основе
         Vector[] vectorDet = {new Vector(det0), new Vector(det1), new Vector(det2), new Vector(det3)};
         Matrix matrix1 = new Matrix(vectorDet);
+
         double x = matrix1.getDet();
+        System.out.println(matrix1.toString());
         System.out.println(x);
         System.out.println(matrix1.toString());
 
+        //Проверка работы метода умножения матрицы на вектор
+        System.out.println("Умножение матрицы на вектор:");
+
+        ////Создание массивов double'ов для матрицы и вектора
         double[] arrayForMatrix1 = {2, -1, 3};
         double[] arrayForMatrix2 = {0, 4, -2};
         double[] arrayForVector = {1, -3, 2};
-        Vector vectorForMultiply = new Vector(arrayForVector);
-        Vector[] vectorArrayForMatrix = {new Vector(arrayForMatrix1), new Vector(arrayForMatrix2)};
-        Matrix matrixForMultiply = new Matrix(vectorArrayForMatrix);
+        Vector vectorForMultiply = new Vector(arrayForVector); //создал вектор, на который буду умножать
+        Vector[] vectorArrayForMatrix = {new Vector(arrayForMatrix1), new Vector(arrayForMatrix2)}; //создал массив векторов для матрицы
+        Matrix matrixForMultiply = new Matrix(vectorArrayForMatrix);// создал матрицу для умножения
+        System.out.println(matrixForMultiply.toString());
+        System.out.println(vectorForMultiply.toString());
         System.out.println(matrixForMultiply.vectorMultiply(vectorForMultiply).toString());
 
-        Matrix forSumMatrix1 = new Matrix(4, 4);
-        Matrix forSumMatrix2 = new Matrix(matrix1);
+        //Проверка метода сложения матриц
+        System.out.println("Сложение матриц:");
+        Matrix forSumMatrix1 = new Matrix(4, 4); // создал матрицу из нулей для сложения
+        Matrix forSumMatrix2 = new Matrix(matrix1); // создал новую матрицу скопировав ее из вычисления определителя
+        System.out.println(forSumMatrix1.toString());
+        System.out.println(forSumMatrix2.toString());
         System.out.println(forSumMatrix1.sum(forSumMatrix2).toString());
 
+        //Проверка метода вычитания матриц
+        System.out.println("Вычитание матриц:");
+        System.out.println(forSumMatrix1.toString());
+        System.out.println(forSumMatrix2.toString());
         System.out.println(forSumMatrix1.sub(forSumMatrix2).toString());
+
+        //Проверка метода статического сложения матриц
+        System.out.println("Статическое сложение матриц:");
+        double[][] arrayForMatrix3 = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+        Matrix staticSumMatrix1 = new Matrix(arrayForMatrix3);
+        double[][] arrayForMatrix4 = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+        Matrix staticSumMatrix2 = new Matrix(arrayForMatrix4);
+        System.out.println(staticSumMatrix1.toString());
+        System.out.println(staticSumMatrix1.toString());
+        System.out.println(Matrix.getSum(staticSumMatrix1, staticSumMatrix2).toString());
+        System.out.println(staticSumMatrix1.toString());// для проверки того, что переданные аргументы не изменились
+        System.out.println(staticSumMatrix1.toString());
+
+        //Проверка метода статического вычитания матриц
+        System.out.println("Статическое вычитание матриц:");
+        System.out.println(staticSumMatrix1.toString());
+        System.out.println(staticSumMatrix1.toString());
+        System.out.println(Matrix.getSub(staticSumMatrix1, staticSumMatrix2).toString());
+        System.out.println(staticSumMatrix1.toString());// для проверки того, что переданные аргументы не изменились
+        System.out.println(staticSumMatrix1.toString());
+
+        //Проверка метода статического умножения матриц
+        System.out.println("Статическое умножение матриц:");
+        double[][] arrayForMatrix5 = {{3, -1, 2}, {0, 1, -3}};
+        Matrix staticMultiplyMatrix1 = new Matrix(arrayForMatrix5);
+        double[][] arrayForMatrix6 = {{-3, 2, 1}, {1, 1, 2}, {2, 3, 0}};
+        Matrix staticMultiplyMatrix2 = new Matrix(arrayForMatrix6);
+        System.out.println(staticMultiplyMatrix1.toString());
+        System.out.println(staticMultiplyMatrix2.toString());
+        System.out.println(Matrix.getMultiply(staticMultiplyMatrix1, staticMultiplyMatrix2).toString());
+        System.out.println(staticMultiplyMatrix1.toString());// для проверки того, что переданные аргументы не изменились
+        System.out.println(staticMultiplyMatrix2.toString());
+        System.out.println(Matrix.getMultiply(staticMultiplyMatrix2, staticMultiplyMatrix1)); // проверка работы исключения
 
     }
 }
--- Matrix/src/academit/kgm/matrix/Matrix.java
+++ Matrix/src/academit/kgm/matrix/Matrix.java
@@ -6,6 +6,8 @@ import academit.kgm.vector.Vector;
 public class Matrix {
     private Vector[] matrixRows;
 
+    // 1. Конструкторы
+
     public Matrix(int rows, int columns) {
         if (rows <= 0) {
             throw new IllegalArgumentException("Количество строк матрицы не может быть меньше или равно 0");
@@ -49,6 +51,10 @@ public class Matrix {
         }
     }
 
+    // 2. Нестатические методы
+
+    // а. Получение размеров, количества строк и количества столбцов матрицы
+
     public Dimension getSize() {
         return new Dimension(this.getNumberOfRows(), this.getNumberOfColumns());
     }
@@ -60,6 +66,8 @@ public class Matrix {
     public int getNumberOfColumns() {
         return this.matrixRows[0].getSize();
     }
+
+    // b. Получение и задание вектора-строки по индексу
 
     public Vector getRow(int rowNumber) {
         if (rowNumber < 0) {
@@ -81,6 +89,8 @@ public class Matrix {
         this.matrixRows[rowNumber] = new Vector(this.getNumberOfColumns(), vector);
     }
 
+    // c. Получение вектора-столбца по индексу
+
     public Vector getColumn(int numberOfColumn) {
         if (numberOfColumn < 0) {
             throw new IllegalArgumentException("Номера запрашиваемого столбца не должен быть меньше 0");
@@ -95,6 +105,8 @@ public class Matrix {
         return new Vector(requiredColumn);
     }
 
+    // d. Транспонирование матрицы
+
     public Matrix transMatrix() {
         Vector[] transMatrix = new Vector[this.getNumberOfColumns()];
         for (int i = 0; i < transMatrix.length; ++i) {
@@ -104,15 +116,7 @@ public class Matrix {
         return this;
     }
 
-
-    public String toString() {
-        StringBuilder stringMatrix = new StringBuilder("{");
-        for (int i = 0; i < this.matrixRows.length - 1; ++i) {
-            stringMatrix.append(this.matrixRows[i].toString()).append(", ");
-        }
-        stringMatrix.append(this.matrixRows[this.matrixRows.length - 1].toString()).append("}");
-        return stringMatrix.toString();
-    }
+    // e. Умножение на скаляр
 
     public Matrix multiply(double x) {
         for (int i = 0; i < this.matrixRows.length; ++i) {
@@ -121,10 +125,14 @@ public class Matrix {
         return this;
     }
 
+    // Метод сравнения 2х double'ов
+
     private static boolean isEqual(double x1, double x2) {
         final double EPSILON = 0.0001;
         return Math.abs(x1 - x2) < EPSILON;
     }
+
+    // f. Вычисление определителя матрицы
 
     public double getDet() {
         if (this.getNumberOfRows() != this.getNumberOfColumns()) {
@@ -161,27 +169,41 @@ public class Matrix {
         return matrixDet * Math.pow(-1, countOfLineTransposition);
     }
 
-    public Matrix vectorMultiply(Vector vector) {
+    // g. Переопределение toString
+
+    public String toString() {
+        StringBuilder stringMatrix = new StringBuilder("{");
+        for (int i = 0; i < this.matrixRows.length - 1; ++i) {
+            stringMatrix.append(this.matrixRows[i].toString()).append(", ");
+        }
+        stringMatrix.append(this.matrixRows[this.matrixRows.length - 1].toString()).append("}");
+        return stringMatrix.toString();
+    }
+
+    // h. умножение матрицы на вектор
+
+    public Vector vectorMultiply(Vector vector) {
         if (this.getNumberOfColumns() != vector.getSize()) {
             throw new IllegalArgumentException("Перемножаться могут только матрицы у которых число столбцов первой совпадает с числом строк второй");
         }
 
-        Matrix tempMatrix = new Matrix(this.getNumberOfRows(), 1);
+        Vector tempVector = new Vector(this.getNumberOfRows());
         double currentNewElement = 0;
         for (int i = 0; i < this.matrixRows.length; ++i) {
             for (int j = 0; j < this.matrixRows[i].getSize(); ++j) {
                 currentNewElement = currentNewElement + this.matrixRows[i].getElement(j) * vector.getElement(j);
             }
-            tempMatrix.matrixRows[i].setElement(0, currentNewElement);
+            tempVector.setElement(i, currentNewElement);
             currentNewElement = 0;
         }
-        this.matrixRows = tempMatrix.matrixRows;
-        return this;
+        return tempVector;
     }
 
+    // i. Сложение матриц
+
     public Matrix sum(Matrix matrix1) {
-        if (this.matrixRows.length != matrix1.matrixRows.length ||
-                this.matrixRows[0].getSize() != matrix1.matrixRows[0].getSize()) {
+        if (this.getNumberOfRows() != matrix1.getNumberOfRows() ||
+                this.getNumberOfColumns() != matrix1.getNumberOfColumns()) {
             throw new IllegalArgumentException("Складываться могут только матрицы одинаковой размерности");
         }
         for (int i = 0; i < this.matrixRows.length; ++i) {
@@ -190,9 +212,11 @@ public class Matrix {
         return this;
     }
 
+    // j. Вычитание матриц
+
     public Matrix sub(Matrix matrix1) {
-        if (this.matrixRows.length != matrix1.matrixRows.length ||
-                this.matrixRows[0].getSize() != matrix1.matrixRows[0].getSize()) {
+        if (this.getNumberOfRows() != matrix1.getNumberOfRows() ||
+                this.getNumberOfColumns() != matrix1.getNumberOfColumns()) {
             throw new IllegalArgumentException("Вычитать можно только матрицы одинаковой размерности");
         }
         for (int i = 0; i < this.matrixRows.length; ++i) {
@@ -200,6 +224,42 @@ public class Matrix {
         }
         return this;
     }
-}
 
+    // 3. Статические методы
+
+    // a. Сложение матриц
+
+    public static Matrix getSum(Matrix matrix1, Matrix matrix2) {
+        Matrix tempMatrix = new Matrix(matrix1);
+        return tempMatrix.sum(matrix2);
+    }
+
+    // b. Вычитание матриц
+
+    public static Matrix getSub(Matrix matrix1, Matrix matrix2) {
+        Matrix tempMatrix = new Matrix(matrix1);
+        return tempMatrix.sub(matrix2);
+    }
+
+    // c. Умножение матриц
+
+    public static Matrix getMultiply(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.getNumberOfColumns() != matrix2.getNumberOfRows()) {
+            throw new IllegalArgumentException("Перемножаться могут только матрицы у которых число столбцов первой совпадает с числом строк второй");
+        }
+
+        Matrix tempMatrix = new Matrix(matrix1.getNumberOfRows(), matrix2.getNumberOfColumns());
+        double currentNewElement = 0;
+        for (int i = 0; i < matrix1.matrixRows.length; ++i) {
+            for (int j = 0; j < matrix2.matrixRows[i].getSize(); ++j) {
+                for (int k = 0; k < matrix2.matrixRows.length; ++k) {
+                    currentNewElement = currentNewElement + matrix1.matrixRows[i].getElement(k) * matrix2.matrixRows[k].getElement(j);
+                }
+                tempMatrix.matrixRows[i].setElement(j, currentNewElement);
+                currentNewElement = 0;
+            }
+        }
+        return tempMatrix;
+    }
+}
 
